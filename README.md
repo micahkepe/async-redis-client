762e492ff670b7faecf43af9959200ffaf326573
micahkepe <micahkepe@gmail.com>
2025-07-05 18:19:23 -0700

    work up to "I/O" chapter

---
+++ README.md
@@ -0,0 +1,11 @@
+# [`tokio`](https://tokio.rs/) Runtime Tutorial
+
+`mini-redis` client tutorial: https://tokio.rs/tokio/tutorial/
+
+## TODO
+
+- [ ] I/O
+- [ ] Framing
+- [ ] Async in depth
+- [ ] Select
+- [ ] Streams
---
+++ examples/hello-redis.rs
@@ -0,0 +1,17 @@
+use mini_redis::{Result, client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Open a connection to mini-redis
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello"
+    client.set("hello", "world".into()).await?;
+
+    // get key
+    let result = client.get("hello").await?;
+
+    println!("got value from server; result={:?}", result);
+
+    Ok(())
+}
